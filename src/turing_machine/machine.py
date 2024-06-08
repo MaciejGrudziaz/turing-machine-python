91ef215b0f3e258bf4e0ece21165e84105067d8a
Barszcz123u <bartoszka05@gmail.com>
2024-06-08 10:25:53 +0200

    Update machine.py

--- src/turing_machine/machine.py
+++ src/turing_machine/machine.py
@@ -1,41 +1,83 @@
-class TuringMachine:
-    def __init__(self, tape, initial_state, final_states, transition_function):
+from abc import ABC, abstractmethod
+
+class Tape:
+    def __init__(self, tape):
         self.tape = list(tape)
         self.head = 0
-        self.state = initial_state
+
+    def move_left(self):
+        self.head -= 1
+
+    def move_right(self):
+        self.head += 1
+
+    def get_value(self):
+        return self.tape[self.head]
+
+    def set_value(self, val):
+        self.tape[self.head] = val
+
+    def __str__(self):
+        return ''.join(self.tape)
+
+
+class TuringMachine(ABC):
+    def __init__(self, tapes, initial_state, final_states):
+        self.initial_tapes = [Tape(tape) for tape in tapes]
+        self.initial_state = initial_state
         self.final_states = final_states
-        self.transition_function = transition_function
+        self.reset()
+
+    def reset(self):
+        self.tapes = [Tape(tape.tape) for tape in self.initial_tapes]
+        self.state = self.initial_state
         self.match_position = -1
 
+    def get_tapes(self):
+        return [tape.tape for tape in self.tapes]
+
+    @abstractmethod
+    def run_auto(self):
+        pass
+
+    def set_tapes(self, new_values):
+        for tape, value in zip(self.tapes, new_values):
+            tape.set_value(value)
+
+    def move_tapes(self, operations):
+        for tape, operation in zip(self.tapes, operations):
+            if operation == 1:
+                tape.move_right()
+            elif operation == -1:
+                tape.move_left()
+
     def step(self):
-        current_char = self.tape[self.head]
-        if (self.state, current_char) in self.transition_function:
-            next_state, write_char, direction = self.transition_function[(self.state, current_char)]
-            self.tape[self.head] = write_char
-            self.state = next_state
-            if direction == 'R':
-                self.head += 1
-            elif direction == 'L':
-                self.head -= 1
-            if self.state in self.final_states:
-                self.match_position = self.head
-        else:
-            raise Exception("Error")
+        tape_state = self.get_tapes()
+        new_state, new_values, operations = self.run_auto()
+        self.state = new_state
+        self.set_tapes(new_values)
+        self.move_tapes(operations)
+        if self.state in self.final_states:
+            self.match_position = [tape.head for tape in self.tapes]
+        return self.state
 
     def run(self):
         while self.state not in self.final_states:
             self.step()
+            self.print_status()
 
-    def get_tape(self):
-        return ''.join(self.tape)
+    def run_tick(self):
+        while self.state not in self.final_states:
+            input("Press Enter to execute the next step...")
+            self.step()
+            self.print_status()
+
+    def print_status(self):
+        tapes_str = ' | '.join([str(tape) for tape in self.tapes])
+        heads_str = ' | '.join([str(tape.head) for tape in self.tapes])
+        print(f"Tapes: {tapes_str}")
+        print(f"Head Positions: {heads_str}")
+        print(f"Current State: {self.state}")
 
     def get_match_position(self):
         return self.match_position
-
-def build_transition_function(substring):
-    transition_function = {}
-    for i, char in enumerate(substring):
-        transition_function[(f'q{i}', char)] = (f'q{i+1}', char, 'R')
-    transition_function[(f'q{len(substring)}', '_')] = (f'q{len(substring)}', '_', 'R')
-    return transition_function
-
